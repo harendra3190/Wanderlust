a0c741dbd9352c11b610b400bb1c226c0676fe78
harendra3130 <prataph506@gmail.com>
2026-07-11 12:56:22 +0530

    [added delete button to reviews]

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const review = require("./review.js");
 
 const listingSchema = new Schema({
   title: {
@@ -30,6 +31,13 @@ const listingSchema = new Schema({
 
 });
 
+listingSchema.post("findOneAndDelete", async (listing)=> {
+  if(listing){
+    await review.deleteMany({_id: { $in: listing.reviews }});
+  }
+  
+
+});
 const Listing = mongoose.model("Listing", listingSchema);
 
 module.exports = Listing;
