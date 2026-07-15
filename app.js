12df854a147e13e5833e6c4ef79e9d2944f5eb07
harendra3130 <prataph506@gmail.com>
2026-07-15 22:22:19 +0530

    [add cokkies , implement session and implement flash]

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const {listingSchema,reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
@@ -28,104 +32,39 @@ main()
     console.log(err);
   });
 
+  const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  };
 
-app.get("/", (req, res) => {
+  app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
-const validateListing=(req,res,next)=>{
-  let{error} = listingSchema.validate(req.body);
-  if(error){
-     let errMsg = error.details.map((el)=>el.message).join(",");
-    throw new ExpressError(400,errMsg);
-  }else{
+  app.use(session(sessionOptions));
+
+  app.use(flash());
+  app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    //console.log(res.locals.success);
+    res.locals.error = req.flash("error");
     next();
-  }
-}
+  });
 
-const validateReview=(req,res,next)=>{
-  let{error} = reviewSchema.validate(req.body);
-  if(error){
-     let errMsg = error.details.map((el)=>el.message).join(",");
-    throw new ExpressError(400,errMsg);
-  }else{
-    next();
-  }
-}
-
-// index routing
-app.get("/listings",wrapAsync( async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-}));
-// new route
-app.get("/listings/new", (req, res) => {
-  res.render("listings/new.ejs");
-});
-// show route
-app.get("/listings/:id",wrapAsync (async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id).populate("reviews");
-  res.render("listings/show.ejs", { listing });
-}));
-
-// create route
-app.post("/listings", validateListing,wrapAsync (async (req, res,next) => {
-  let result=listingSchema.validate(req.body);
-  console.log(result);
-  if(result.error){
-    throw new ExpressError(400,result.error);
-  }
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-  
  
-  
-}));
-// Edit route
-app.get("/listings/:id/edit",wrapAsync (async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-}));
-// update route
-app.put("/listings/:id",validateListing,wrapAsync( async (req, res) => {
-  
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-}));
-
-//delete
-
-app.delete("/listings/:id", wrapAsync(async (req, res) => {
-  let { id } = req.params;
-  let deletedListings = await Listing.findByIdAndDelete(id);
-  console.log(deletedListings);
-  res.redirect("/listings");
-}));
 
 
-//reviews 
-app.post("/listings/:id/reviews",validateReview,wrapAsync(async (req, res) => {
-     let listing = await Listing.findById(req.params.id);
-     let newReview = new Review(req.body.review);
-     listing.reviews.push(newReview);
-     await newReview.save();
-     await listing.save();
-     console.log("new review saved");
-     res.send(" new review added");
-     
-}));
 
-// delete review route
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async (req,res)=>{
-  let {id,reviewId} = req.params;
-  await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/listings/${id}`);
-}));
+app.use("/listings",listings);
+
+
+app.use("/listings/:id/reviews", reviews);
 
 
 app.use((req, res, next) => {
@@ -141,3 +80,4 @@ app.use((err,req,res,next)=>{
 app.listen(8080, () => {
   console.log("server is listening on port 8080");
 });
+ 
